50d5716ed0ad6e0dd7a5242adf4e6b1a9c044b1a
Rümeysa Ceylan <rumeysaceylan0619@gmail.com>
2022-08-01 13:49:06 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login UI',
+      title: 'Flutter Login',
       debugShowCheckedModeBanner: false,
       home: MyApp(),
     );
